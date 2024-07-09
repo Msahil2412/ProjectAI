564d692ba7caa105fb34359e939a37414196364e
Msahil2412 <sahil2410khan@gmail.com>
2024-07-09 12:24:42 +0530

    modified:   PyCodes/TRANSFORMER.py

---
+++ PyCodes/TRANSFORMER.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import transformers
+from transformers import AutoModel, BertTokenizerFast
+
+device = torch.device("cuda")
